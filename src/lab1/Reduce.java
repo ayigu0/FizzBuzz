34ad715d020bd865a717744f5923b404c9faeccf
ayigu0 <michaelqian225@gmail.com>
2025-09-08 17:08:43 -0400

    test with candice

--- src/lab1/Reduce.java
+++ src/lab1/Reduce.java
@@ -2,11 +2,10 @@ package lab1;
 
 public class Reduce {
     public static void main(String[] args) {
-        System.out.println(reduce());
+        System.out.println(reduce(5));
     }
 
     public static int reduce(int n){
-        int count = 0;
-        while()
+
     }
 }
