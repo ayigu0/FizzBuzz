3fa2b3a8134474ee3fe8a7d809850e7afecdabd2
ayigu0 <michaelqian225@gmail.com>
2025-09-08 16:49:00 -0400

    second attemptqwe

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce());
+    }
+
+    public static int reduce(int n){
+        int count = 0;
+        while()
+    }
+}
