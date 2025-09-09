a574a2bb9ac4ba0dfcae571ce3e467a86e750a93
ayigu0 <michaelqian225@gmail.com>
2025-09-08 20:16:15 -0400

    Update Main.java

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -2,7 +2,9 @@ package lab1;
 //12321312
 public class Main {
     public static void main(String[] args) {
-    //testtest
+    
     }
-
+    private static void f(){
+        return 10;
+    }
 }
